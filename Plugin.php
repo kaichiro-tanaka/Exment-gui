00da8137509058b462955838b3f536c25700869e
kaichiro.tanaka <kaichiro.tanaka@cisys.co.jp>
2021-07-26 15:37:54 +0900

    test

--- Plugin.php
+++ Plugin.php
@@ -1,141 +1,124 @@
 <?php
 
 // (1)
-namespace App\Plugins\ManagementView;
+namespace App\Plugins\YouTubeSearch;
 
-use Exceedone\Exment\Services\Plugin\PluginViewBase;
-use Exceedone\Exment\Enums\ColumnType;
-use Exceedone\Exment\Model\CustomColumn;
+use Encore\Admin\Widgets\Box;
+use Exceedone\Exment\Model\CustomTable;
+use Exceedone\Exment\Services\Plugin\PluginPageBase;
+use GuzzleHttp\Client;
 
-class Plugin extends PluginViewBase
+class Plugin extends PluginPageBase
 {
-    // (3) プラグイン独自の設定を追加する場合はコメントアウト
-    // protected $useCustomOption = true;
+    // (3)
+    protected $useCustomOption = true;
 
     /**
-     * (2) 一覧表示時のメソッド。"grid"固定
-     */
-    public function grid()
-    {
-        $values = $this->values();
-        // (5) ビューを呼び出し
-        return $this->pluginView('sample', ['values' => $values]);
-    }
-
-    /**
-     * (2) このプラグイン独自のエンドポイント
-     */
-    public function update(){
-        $value = request()->get('value');
-
-        $custom_table = CustomTable::getEloquent(request()->get('table_name'));
-        $custom_value = $custom_table->getValueModel(request()->get('id'));
-
-        $custom_value->setValue($value)
-            ->save();
-
-        return response()->json($custom_value);
-    }
-
-    /**
-     * (3) ビュー設定画面で表示するオプション
-     * Set view option form for setting
+     * (2) Index
      *
-     * @param Form $form
      * @return void
      */
-    public function setViewOptionForm($form)
+    public function index()
     {
-        //　独自設定を追加する場合
-        $form->embeds('custom_options', '詳細設定', function($form){
-            $form->select('category', 'カテゴリ列')
-                ->options($this->custom_table->getFilteredTypeColumns([ColumnType::SELECT, ColumnType::SELECT_VALTEXT])->pluck('column_view_name', 'id'))
-                ->required()
-                ->help('カテゴリ列を選択してください。カンバンのボードに該当します。カスタム列種類「選択肢」「選択肢(値・見出し)」が候補に表示されます。');
-        });
-
-        //　フィルタ(絞り込み)の設定を行う場合
-        static::setFilterFields($form, $this->custom_table);
-
-        // 並べ替えの設定を行う場合
-        static::setSortFields($form, $this->custom_table);
+        return $this->getIndexBox();
     }
 
+    /**
+     * (2) youTube結果一覧表示
+     *
+     * @return void
+     */
+    public function list()
+    {
+        $html = $this->getIndexBox()->render();
+
+        // 文字列検索
+        $client = new Client([
+            'base_uri' => 'https://www.googleapis.com/youtube/v3/',
+        ]);
+
+        $method = 'GET';
+        $uri = "search?part=id&type=video&maxResults=20&key=" . $this->plugin->getCustomOption('access_key') 
+            . "&q=" . urlencode(request()->get('youtube_search_query')); //検索
+        $options = [];
+        $response = $client->request($method, $uri, $options);
+
+        $list = json_decode($response->getBody()->getContents(), true);
+        $ids = collect(array_get($list, 'items', []))->map(function($l){
+            return array_get($l, 'id.videoId');
+        })->toArray();
+
+
+        // idより詳細を検索
+        $client = new Client([
+            'base_uri' => 'https://www.googleapis.com/youtube/v3/',
+        ]);
+
+        $method = 'GET';
+        $uri = "videos?part=id,snippet,statistics&key=" . $this->plugin->getCustomOption('access_key') 
+            . "&id=" . implode(',', $ids); //検索
+        $options = [];
+        $response = $client->request($method, $uri, $options);
+
+        $list = json_decode($response->getBody()->getContents(), true);
+
+        $html .= new Box("YouTube検索結果", view('exment_you_tube_search::list', [
+            'items' => array_get($list, 'items', []),
+            // (5)
+            'item_action' => $this->plugin->getRouteUri('save'),
+        ])->render());
+
+        return $html;
+    }
 
     /**
-     * (4) プラグインの編集画面で設定するオプション。全ビュー共通で設定する
+     * (2) データ保存
+     *
+     * @return void
+     */
+    public function save(){
+        $request = request();
+        $model = CustomTable::getEloquent('youtube')->getValueModel();
+        $model->setValue('youtubeId', $request->get('youtubeId'));
+        $model->setValue('description', $request->get('description'));
+        $model->setValue('viewCount', $request->get('viewCount'));
+        $model->setValue('likeCount', $request->get('likeCount'));
+        $model->setValue('dislikeCount', $request->get('dislikeCount'));
+        $model->setValue('url', $request->get('url'));
+        $model->setValue('title', $request->get('title'));
+        $model->setValue('publishedAt', $request->get('publishedAt'));
+        $model->save();
+
+        admin_toastr(trans('admin.save_succeeded'));
+        return redirect()->back()->withInput();
+    }
+
+    /**
+     * 検索ボックス取得
+     *
+     * @return void
+     */
+    protected function getIndexBox(){
+        // (3) YouTube アクセスキーのチェック
+        $hasKey = !is_null($this->plugin->getCustomOption('access_key'));
+
+        // (4)
+        return new Box("YouTube検索", view('exment_you_tube_search::index', [
+            'action' => $this->plugin->getRouteUri('list'),
+            'youtube_search_query' => request()->get('youtube_search_query'),
+            'hasKey' => $hasKey,
+        ]));
+    }
+
+    /**
+     * (3) プラグインの編集画面で設定するオプション。アクセスキーを入力させる
      *
      * @param [type] $form
      * @return void
      */
     public function setCustomOptionForm(&$form)
     {
-        // 必要な場合、追加
-        // $form->text('access_key', 'アクセスキー')
-        //     ->help('YouTubeのアクセスキーを入力してください。');
-    }
-
-
-    // 以下、かんばんビューで必要な処理 ----------------------------------------------------
-
-    protected function values(){
-        $query = $this->custom_table->getValueQuery();
-
-        // データのフィルタを実施
-        $this->custom_view->filterModel($query);
-
-        // データのソートを実施
-        $this->custom_view->sortModel($query);
-
-        // 値を取得
-        $items = collect();
-        $query->chunk(1000, function($values) use(&$items){
-            $items = $items->merge($values);
-        });
-
-        $boards = $this->getBoardItems($items);
-
-        return $boards;
-    }
-
-
-    protected function getBoardItems($items){
-        $category = CustomColumn::getEloquent($this->custom_view->getCustomOption('category'));
-        $options = $category->createSelectOptions();
-
-        // set boards
-        $boards_dragTo = collect($options)->map(function($option, $key){
-            return "board-id-$key";
-        })->toArray();
-
-        $boards = collect($options)->map(function($option, $key) use($category, $boards_dragTo){
-            return [
-                'id' => "board-id-$key",
-                'column_name' => $category->column_name,
-                'key' => $key,
-                'title' => $option,
-                'drapTo' => $boards_dragTo,
-                'item' => [],
-            ];
-        })->values()->toArray();
-
-        foreach($items as $item){
-            $c = array_get($item, 'value.' . $category->column_name);
-
-            foreach($boards as &$board){
-                if(!isMatchString($c, $board['key'])){
-                    continue;
-                }
-
-                $board['item'][] = [
-                    'id' => "item-id-$item->id",
-                    'title' => $item->getLabel(),
-                    'dataid' => $item->id,
-                    'table_name' => $this->custom_table->table_name
-                ];
-            }
-        }
-
-        return $boards;
+        $form->text('access_key', 'アクセスキー')
+            ->help('YouTubeのアクセスキーを入力してください。');
     }
 }
